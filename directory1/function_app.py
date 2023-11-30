31cecf28b153bc37fe2cd37705d2fbe857bc10e8
LeaL <LeaL@skyvar.co.il>
2023-11-30 16:47:36 +0200

    1152

--- directory1/function_app.py
+++ directory1/function_app.py
@@ -16,6 +16,5 @@ def test_function(mytimer: func.TimerRequest) -> None:
 
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
 
