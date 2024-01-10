81fa873a4b1191ecc5953062535e1ee0f9818d3c
Y. Charaz <yeuditc@skyvar.co.il>
2024-01-10 10:03:57 +0200

    Come on, work alreadygit add . git add .

--- services/emails/func-minus/function_app.py
+++ services/emails/func-minus/function_app.py
@@ -11,7 +11,6 @@ def test_function(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
-
     if mytimer.past_due:
         logging.info('The timer is past due!!')
 
