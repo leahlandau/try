c408128b7abb49056175dfa4b51c6a169a623e58
LeaL <LeaL@skyvar.co.il>
2024-02-14 14:36:02 +0200

    :terraform: change directory

--- services/emails/deployment-functionapps/config/__init__.py
+++ services/emails/deployment-functionapps/config/__init__.py
@@ -2,3 +2,23 @@
 # ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
 # need added code
 
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
