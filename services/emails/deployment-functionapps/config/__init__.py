92fa8e46b1e4462e4821bc57cd2c6b4140cefa9a
LeaL <LeaL@skyvar.co.il>
2024-02-14 14:06:25 +0200

    :terraform: change directory

--- services/emails/deployment-functionapps/config/__init__.py
+++ services/emails/deployment-functionapps/config/__init__.py
@@ -1,4 +1,3 @@
 # need added code
 # ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
 # tfghjggfxgcvhgcfdxfcghvbjgftgtghjmk
-# fghjkjkhgfghvjbkhgytfhmjkhklyg6ftgvcbhfynjuhghfgdrfdrgtfhgtyujiuhgyuhtygrterfgjhjkn,jnkhtfrdfgchvjgftdfghvjbjhgvftrdtfghjgftrygh
