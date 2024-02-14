3716974e4151a7eb2b063e3098bea27131512025
LeaL <LeaL@skyvar.co.il>
2024-02-14 15:38:29 +0200

    :terraform: change directory

--- services/emails/deployment-functionapps/config/__init__.py
+++ services/emails/deployment-functionapps/config/__init__.py
@@ -9,3 +9,58 @@
 
 
 
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+
+
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+
+
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+
+
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+
+
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+# need added code
+# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
+# need added code
+
+
+
+
