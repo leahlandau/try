e0129b20824fdb164b59f6aed187dd7ef3b42f31
LeaL <LeaL@skyvar.co.il>
2024-02-15 10:52:34 +0200

    deploy

--- services/emails/deployment-functionapps/config/__init__.py
+++ services/emails/deployment-functionapps/config/__init__.py
@@ -9,7 +9,7 @@
 # ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
 # need added code
 
-
+# .nbhdgvxbjfhkiodfugkhbnniuoghkjnm
 
 
 
--- services/emails/func-emails/config/__init__.py
+++ services/emails/func-emails/config/__init__.py
@@ -1,3 +1,4 @@
 # need added code
 # 
-# ythgtrygfhghtyfghjtyrgfhjtyr6tyfgtru6
+# ythgtrygfhghtyfghjtyrgfhjtyr6tyfgtru6kvjmnbcx,klvbinkj,vbmgck.kljbnmgfg,kjmnf,kjlgnmhn,jkntmg,fghklbn
+# jhjnbmdhjfghfxkdhjgbndmjhkfmn 
