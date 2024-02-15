46f5dbdfddebf6c1597f09760a6bdaf675e06b4a
LeaL <LeaL@skyvar.co.il>
2024-02-15 13:04:44 +0200

    deploy

--- services/subscriptions/func-subscriptions-automation/config/__init__.py
+++ services/subscriptions/func-subscriptions-automation/config/__init__.py
@@ -17,23 +17,6 @@
 # ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
 # need added code
 
-# need added code
-# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-fil
-
-
-
-
-
-
-
-# .,gnjfdnmbdn,jklofifghjkbnm,rklkogiuhubjk nvm,# need added code
-# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
-# need added code
-
-# need added code
-# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-fil
-
-
 
 
 
