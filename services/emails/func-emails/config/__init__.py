8fbc5f03bf8512d18a6026cca7b9fc7ac470727e
LeaL <LeaL@skyvar.co.il>
2024-02-15 14:13:11 +0200

    deploy

--- services/emails/func-emails/config/__init__.py
+++ services/emails/func-emails/config/__init__.py
@@ -45,7 +45,7 @@
 
 # .,gnjfdnmbdn,jklofifghjkbnm,rklkogiuhubjk nvm,# need added code
 # ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}
-# need added code
+# need added 
 
 # need added code
 
--- services/subscriptions/func-subscriptions-automation/config/__init__.py
+++ services/subscriptions/func-subscriptions-automation/config/__init__.py
@@ -8,5 +8,4 @@
 # ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-fil
 # ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-fil
 # ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-fil
-# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-fil
 
