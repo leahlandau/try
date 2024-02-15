43c0320b31903201f54c9303482509666780395d
LeaL <LeaL@skyvar.co.il>
2024-02-15 13:48:05 +0200

    deploy

--- services/emails/deployment-functionapps/config/__init__.py
+++ services/emails/deployment-functionapps/config/__init__.py
@@ -90,7 +90,6 @@
 # ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-fil
 # ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-fil
 # ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-fil
-# ${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-files.outputs.all_changed_files }}${{ steps.changed-fil
 
 
 
