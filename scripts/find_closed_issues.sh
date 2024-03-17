e55d5557e70eec5d612c108e300100862c18af19
LeaL <LeaL@skyvar.co.il>
2024-03-17 13:36:54 +0200

    workflow

--- scripts/find_closed_issues.sh
+++ scripts/find_closed_issues.sh
@@ -6,18 +6,18 @@ CURLARGS="-s -H"
 echo $(curl $CURLARGS "$AUTHORIZE" "$URL/releases" | jq -r '.[] | "\(.tag_name)\t\(.created_at)"' | sort -k2,2r
 previous_release_created_at=$(curl $CURLARGS "$AUTHORIZE" "$URL/releases" | jq -r '.[] | "\(.tag_name)\t\(.created_at)"' | sort -k2,2r | awk 'NR==2 {print $2}')
 echo $previous_release_created_at
-issues=$(curl $CURLARGS "$AUTHORIZE" "$URL/issues?state=closed&per_page=100&since=$previous_release_created_at&until=$(date -u +'%Y-%m-%dT%H:%M:%SZ')")
-issues_list=""
-while IFS= read -r row; do
-    number=$(echo "$row" | jq -r '.number')
-    title=$(echo "$row" | jq -r '.title')
-    assignees=$(curl $CURLARGS "$AUTHORIZE" "$URL/issues/$number" | jq -r '.assignees[].login ' | xargs)
-    url=$(echo "$row" | jq -r '.html_url')
-    assignee_links=""
-    for assignee in $assignees; do
-        assignee_links="$assignee_links[@$assignee](https://github.com/$assignee) "
-    done
-    issues_list="${issues_list}- $title in [#$number]($url) by $assignee_links\n"
-done < <(echo "$issues" | jq -c '.[]')
-issues=$(echo "$issues" | jq -c '.[]')
+issues=$(curl $CURLARGS "$AUTHORIZE" "$URL/issues?state=closed&per_page=100&since=$previous_release_created_at&until=$(date -u +'%Y-%m-%dT%H:%M:%SZ')" | jq -c '.[]')
+# issues_list=""
+# while IFS= read -r row; do
+#     number=$(echo "$row" | jq -r '.number')
+#     title=$(echo "$row" | jq -r '.title')
+#     assignees=$(curl $CURLARGS "$AUTHORIZE" "$URL/issues/$number" | jq -r '.assignees[].login ' | xargs)
+#     url=$(echo "$row" | jq -r '.html_url')
+#     assignee_links=""
+#     for assignee in $assignees; do
+#         assignee_links="$assignee_links[@$assignee](https://github.com/$assignee) "
+#     done
+#     issues_list="${issues_list}- $title in [#$number]($url) by $assignee_links\n"
+# done < <(echo "$issues" | jq -c '.[]')
+# issues=$(echo "$issues" | jq -c '.[]')
 curl -X PATCH -H "$AUTHORIZE" -d '{"body": "Whats Changed:\n\n'"$issues"'"}' "$CURRENT_RELEASE_PATH"
