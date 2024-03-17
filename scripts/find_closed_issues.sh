48f8fc1fcb09e646db3e5b59c11154c0c548343e
LeaL <LeaL@skyvar.co.il>
2024-03-17 09:52:12 +0200

    workflow

--- scripts/find_closed_issues.sh
+++ scripts/find_closed_issues.sh
@@ -3,7 +3,8 @@ CURRENT_RELEASE_PATH=$2
 URL="https://api.github.com/repos/${GITHUB_REPOSITORY}"
 AUTHORIZE="Authorization: Bearer $GITHUB_TOKEN"
 CURLARGS="-s -H"
-echo $(curl $CURLARGS "$AUTHORIZE" "$URL/releases" | jq -r '.[] | "\(.tag_name)\t\(.created_at)"' | sort -k2,2r )
+releases=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" "$URL/releases" | jq -r '.[] | "\(.tag_name)\t\(.created_at)"' | sort -t. -k1,1Vr -k2,2Vr -k3,3Vr -k4,4r)
+echo "releases---2 $releases"
 previous_release_created_at=$(curl $CURLARGS "$AUTHORIZE" "$URL/releases" | jq -r '.[] | "\(.tag_name)\t\(.created_at)"' | sort -k2,2r | awk 'NR==2 {print $2}')
 echo $previous_release_created_at
 issues=$(curl $CURLARGS "$AUTHORIZE" "$URL/issues?state=closed&per_page=100&since=$previous_release_created_at&until=$(date -u +'%Y-%m-%dT%H:%M:%SZ')")
