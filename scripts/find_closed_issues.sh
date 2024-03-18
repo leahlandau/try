d695d5b1bffbb4bf25b37c8c2075c8567e9352c8
LeaL <LeaL@skyvar.co.il>
2024-03-18 11:13:25 +0200

    workflow

--- scripts/find_closed_issues.sh
+++ scripts/find_closed_issues.sh
@@ -40,4 +40,6 @@ curl -X PATCH -H "$AUTHORIZE" -d "{\"body\": \"Whats Changed:\\n\\n$(echo "$issu
           echo "issues - $issues"
           issue_list=$(echo "$issues" | jq -r '.[] | "- \(.title) in [#\(.number)](\(.html_url)) by [@\(.assignee.login)](https://github.com/\(.assignee.login))"' | paste -sd '\n' -)
           echo "issue_list - $issue_list"
-          curl -X PATCH -H "$AUTHORIZE" -d "{\"body\": \"Whats Changed:\\n\\n$issue_list\"}" "$CURRENT_RELEASE_PATH"
+          escaped="$(printf '%s' "$issue_list" | jq -Rs '.')"
+          echo "issue_list - $issue_list"
+          curl -X PATCH -H "$AUTHORIZE" -d "{\"body\": \"Whats Changed:\\n\\n$escaped\"}" "$CURRENT_RELEASE_PATH"
